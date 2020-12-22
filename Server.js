da7852b3536fbbf462a5a591ca052fde90922b32
s0121219 <67987849+s0121219@users.noreply.github.com>
2020-12-23 07:45:08 +0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,51 +1,76 @@
 const express = require('express');
 const app = express();
-
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
 
 var msg='';
-var user='';
+
+const SECRETKEY = 'John';
+
 const accounts = [
-	{name: 'demo', password: ''},
-	{name: 'student', password: ''}
+	{id: 'demo', password: ''},
+	{id: 'student', password: ''}
 ];
 
-const authenticateUser = (id = null, password = null) => {
-if(id=='' && password==''){
-	msg='Incorrect userid or password!';
-	return true;
-}
-for(let x of accounts){
-	if(x.name==id && x.password==password){
-	msg='';
-	user=id;
-	return false;
-	}
-}
-return true;
-}
 
-/*let today = new Date();
-    let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello there!';
-    if (includeTime) {
-      msg += `  It is now ${today.toTimeString()}`;
-    }
-    return(msg);*/
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Cookie session for storing login info
+app.use(session({
+  name: 'loginSession',
+  keys: [SECRETKEY]
+}));
 
 app.set('view engine', 'ejs');
 
+
 app.get("/", (req,res) => {
-	res.status(200).render("login",{msg:msg});	
+	console.log(req.session);
+	if (!req.session.authenticated) {    // user not logged in
+		res.status(200).render("login",{msg:msg});
+	} else {
+		res.redirect('/read');
+	}	
 });
 
-app.get("/processlogin", (req,res) => {
-	if(authenticateUser(req.query.id,req.query.password)){
+app.post('/', (req,res) => {
+
+	accounts.forEach((account) => {
+		if (account.id == req.body.id && account.password == req.body.password) {
+			// correct user name + password
+			// store the following name/value pairs in cookie session
+			req.session.authenticated = true;        // 'authenticated': true
+			req.session.id = req.body.id;	 // 'id': req.body.id
+			msg='';		
+		}
+	});
+	if(!req.session.authenticated){
+		msg='Wrong userid or password. Please try again.';
+	}
+	if(req.body.id=='' && req.body.password==''){
+	msg='Please enter your userid or password!';
+	}
 	res.redirect('/');
-	}else
-	res.redirect('/read');
 });
 
 app.get("/read", (req,res) => {
-	res.status(200).render("read",{user:user});	
+	res.status(200).render("read",{user:req.session.id});	
+});
+
+app.get("/createRestaurant", (req,res) => {
+	res.status(200).render("create",{});	
+});
+
+app.post("/createRestaurant", (req,res) => {
+	
+});
+
+app.get('/logout', (req,res) => {
+	req.session = null;   // clear cookie-session
+	res.redirect('/');
 });
 app.listen(process.env.PORT || 8099);
 
